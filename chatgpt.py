1102d4c554a683461f3de09866d73865eb86df85
theVoogie <david.jaeren@webstep.no>
2023-07-10 00:39:34 +0200

    using LangChain to play with data, bot personal and the one from ChatGPT

---
+++ chatgpt.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+import constants;
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
+
+
+key = constants.APIKEY
+query = sys.argv[1]
+
+
+# loader = TextLoader('data.txt')
+loader = DirectoryLoader('./collection', glob='*.txt')
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+print(index.query(query, llm=ChatOpenAI()))
