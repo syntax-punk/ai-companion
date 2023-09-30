398e1be0e035ee26bfa58826a3a5be9cfb30cc2e
theVoogie <david.jaeren@webstep.no>
2023-09-30 14:37:46 +0200

    Added venv

--- chatgpt.py
+++ chatgpt.py
@@ -1,16 +1,13 @@
-import os
 import sys
 
-import constants;
+from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+load_dotenv()
 
-
-os.environ['OPENAI_API_KEY'] = constants.APIKEY
-key = constants.APIKEY
 query = sys.argv[1]
 
 
